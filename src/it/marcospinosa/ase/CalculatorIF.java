a86ac3d5e0e0f1f52be7eaab2d92d3682ec3d651
marcoSpinosa <marcospinosa@live.it>
2017-09-22 18:25:23 +0200

    Fixed

--- src/it/marcospinosa/ase/CalculatorIF.java
+++ src/it/marcospinosa/ase/CalculatorIF.java
@@ -2,5 +2,7 @@ package it.marcospinosa.ase;
 
 public interface CalculatorIF {
     int sum(int m, int n);
+    int subtract(int m, int n);
+    int multiply(int m, int n);
     int divide(int m, int n);
 }
--- src/it/marcospinosa/ase/CalculatorPrototype.java
+++ src/it/marcospinosa/ase/CalculatorPrototype.java
@@ -3,21 +3,63 @@ package it.marcospinosa.ase;
 public class CalculatorPrototype implements CalculatorIF {
     @Override
     public int sum(int m, int n) {
-        int count = n<0 ? -n : n;
+        if (n == 0) {
+            return m;
+        }
+        if (m == 0) {
+            return n;
+        }
+        int count = Math.abs(n);
         boolean isNegative = n<0;
         int sum = m;
         for (int i = 0; i<count; i++){
             if (isNegative) {
-                sum -=1;
+                sum -= 1;
             } else {
-                sum+=1;
+                sum += 1;
             }
         }
         return sum;
     }
 
     @Override
+    public int subtract(int m, int n) {
+        return sum(m, -n);
+    }
+
+    @Override
+    public int multiply(int m, int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int nSign = n<0 ? -1 : 1;
+        int mSign = m<0 ? -1 : 1;
+        int nn = Math.abs(n);
+        int mm = Math.abs(m);
+        return nSign * mSign * positiveMultiply(mm, nn);
+    }
+
+    @Override
     public int divide(int m, int n) {
+        if (n == 0) {
+            throw new CalculatorException("You cannot divide by zero!");
+        }
+        int nSign = n<0 ? -1 : 1;
+        int mSign = m<0 ? -1 : 1;
+        int nn = Math.abs(n);
+        int mm = Math.abs(m);
+        return nSign * mSign * positiveDivide(mm, nn);
+    }
+
+    private int positiveMultiply(int m, int n){
+        int res = 0;
+        for (int i=0; i<n; i++){
+            res += m;
+        }
+        return res;
+    }
+
+    private int positiveDivide(int m, int n){
         if (n > m) {
             return 0;
         }
@@ -26,8 +68,8 @@ public class CalculatorPrototype implements CalculatorIF {
         }
         int count = 0;
         boolean stop = false;
-        while(m > 0 && !stop) {
-            if (m-n >= 0){
+        while(m> 0 && !stop) {
+            if (m-n >= 0) {
                 m -= n;
                 count++;
             } else {
@@ -35,5 +77,11 @@ public class CalculatorPrototype implements CalculatorIF {
             }
         }
         return count;
+    }
+
+    static public class CalculatorException extends RuntimeException {
+        public CalculatorException(String message) {
+            super(message);
+        }
     }
 }
